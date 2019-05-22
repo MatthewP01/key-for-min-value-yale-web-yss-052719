6ff8cedbb2805d05ef9ec0a395c1805da58af861
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 23:12:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,7 +9,7 @@ def key_for_min_value(name_hash)
 
 def key_for_min_value(name_hash)
   smallest_name = nil
-  smallest = 0
+  smallest = nil
 
   name_hash.each do |key, value|
     if value <= smallest
