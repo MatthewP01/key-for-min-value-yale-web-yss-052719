08435a40e74f2cfdb6280b98f9f0d39959e65f18
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 23:15:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -11,7 +11,7 @@ def key_for_min_value(name_hash)
   lowkey = nil
   lowval = nil
   name_hash.each do |key, value|
-  if lowest_value == nil || v < lowest_value
+  if lowval == nil || value < lowval
     lowval = v
     lowkey = k
   end
